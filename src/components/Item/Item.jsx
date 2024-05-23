aa8bfc27888e44ae799ff6f6dcb8f818bb049ba4
Piccoli Augusto <piccoli_44@hotmail.com>
2024-05-23 17:55:29 -0300

    Boton de compra y agregar a carrito para box de productos

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,4 +1,4 @@
-import { Box, Image, Text } from '@chakra-ui/react'
+import { Box, Image, Text, Divider, ButtonGroup, Button } from '@chakra-ui/react'
 import React from 'react'
 
 const Item = ({ marca, modelo, precio, img }) => {
@@ -6,7 +6,7 @@ const Item = ({ marca, modelo, precio, img }) => {
     <Box 
       border="1px solid #E0E0E0"
       borderRadius="10px"
-      p={4}
+      p={3}
       transition="transform 0.3s, box-shadow 0.3s"
       _hover={{ transform: "translateY(-5px)", boxShadow: "lg", cursor: "Pointer" }}
       textAlign="center"
@@ -28,9 +28,18 @@ const Item = ({ marca, modelo, precio, img }) => {
       <Text fontFamily="Montserrat, sans-serif" fontWeight="500" color="gray.500">
         {modelo}
       </Text>
-      <Text fontFamily="Montserrat, sans-serif" fontWeight="bold" color="primary" mt={2}>
+      <Text fontFamily="Montserrat, sans-serif" fontWeight="bold" fontSize={"18px"} color="primary" mt={2}>
         ${precio}
       </Text>
+      <Divider />
+      <ButtonGroup spacing="2" mt={3}>
+        <Button size="sm" bg="#FF6F00" color="white" _hover={{ bg: "#E65C00", boxShadow: "2px 2px 6px #aaa" }} _active={{transform: "scale(.9)"}}>
+          Comprar
+        </Button>
+        <Button size="sm" variant="ghost" colorScheme='orange' _active={{transform: "scale(.9)"}}>
+          Agregar al Carrito
+        </Button>
+      </ButtonGroup>
     </Box>
   )
 }
--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -7,7 +7,7 @@ const ItemList = ({productos}) => {
     
 
   return (
-    <SimpleGrid columns={[1, 2, 3, 4]} spacing={10} m={2}> 
+    <SimpleGrid columns={[1, 2, 3, 4, 5]} spacing={10} m={2}> 
         {
             productos.map((prod) => (
               <Box key={prod.id}>
